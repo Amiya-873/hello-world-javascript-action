1b7fdb9bffee85cf0d3965771c5849747d143685
Amiya-873 <71648041+Amiya-873@users.noreply.github.com>
2023-09-19 13:04:55 +0530

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 const sinon = require('sinon') // Assuming you have Sinon installed
 const chai = require('chai')
 const expect = chai.expect
+const assert = require('assert');
 const chaiaspromised = require('chai-as-promised')
 chai.use(chaiaspromised)
 
@@ -8,18 +9,38 @@ const { triggerAPI } = require('../index')
 
 describe('Unit Testing', function(){
 
+  let clock;
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(() => {
+    clock.restore();
+  });
+  
   it('should trigger the API to make a deploy request at least once', function() {
     const triggerAPISpy = sinon.spy(triggerAPI) 
-    triggerAPI()
-    sinon.assert.calledOnce(triggerAPISpy)    
+    
+    setTimeout(() => {
+      triggerAPI()
+      sinon.assert.calledOnce(triggerAPISpy)
+    },0) 
   })
 
-
   it('should trigger the API to make a deploy request and handle successful response', function() {
-    expect(triggerAPI()).to.equal('Status is SUCCEEDED. Breaking the loop.') 
+    let result = ''
+    setTimeout(() => {
+      result = triggerAPI()
+      expect.result.to.equal('Status is SUCCEEDED. Breaking the loop.')
+    },0)
   })
 
   it('should trigger the API to make a deploy request and handle failure response', function() {
-    expect(triggerAPI()).to.equal('Deployment failed in UCD')
+    let result = ''
+    setTimeout(() => {
+      result = triggerAPI()
+      expect.result.to.equal('Deployment failed in UCD')
+    },0)
   })
 })
